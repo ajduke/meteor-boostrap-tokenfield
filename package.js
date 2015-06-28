297b8e3e8cd6b7554182c2583c134d0d0de597c1
ajduke <ajduke@about.me>
2015-06-28 09:51:02 +0530

    updated to official bootstrap repo

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     name: "ajduke:bootstrap-tokenfield",
     summary: "A jQuery tag/token input plugin for Twitter's Bootstrap, repackaged for Meteor.",
-    version: "0.4.0",
+    version: "0.5.0",
     git: "https://github.com/ajduke/meteor-boostrap-tokenfield"
 });
 
 Package.on_use(function (api) {
-    api.use('mizzao:bootstrap-3@3.3.1', 'client');
+    api.use('twbs:bootstrap@3.3.5', 'client');
 
     api.add_files('lib/bootstrap-tokenfield/dist/bootstrap-tokenfield.min.js', 'client');
     api.add_files('lib/bootstrap-tokenfield/dist/css/tokenfield-typeahead.min.css', 'client');
